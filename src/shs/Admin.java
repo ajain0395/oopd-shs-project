e06c44ea2307bc1586a74a300acda78715a4481e
Meenakshi Maindola <meenakshi18081@iiitd.ac.in>
2018-11-15 18:31:42 +0530

    admin fixes and features

--- src/shs/Admin.java
+++ src/shs/Admin.java
@@ -2,7 +2,6 @@ package shs;
 
 import java.sql.*;
 import java.util.*;
-import java.util.Date;
 
 public class Admin extends Person 
 {
@@ -10,10 +9,22 @@ public class Admin extends Person
 	Scanner sc=new Scanner(System.in);
 	Credentials loginCredentials;
 	public Admin() {
+		loginCredentials = new Credentials();
 		// TODO Auto-generated constructor stub
 		this.loginCredentials.setId("ADMIN@123");
 		this.loginCredentials.setPassword("admin@123");
 		
+		
+	}
+	int adminLogin(){
+		System.out.println("Enter Username: ");
+		String u=SmartHealthCareSystem.nextintString();
+		System.out.println("Enter Password: ");
+		String p=SmartHealthCareSystem.nextintString();
+		if(u=="ADMIN@123" && p=="admin@123")
+			return 1;
+		else 
+			return 0;
 	}
 	
 	void getDoctorDetails()
@@ -22,42 +33,48 @@ public class Admin extends Person
 		int i=1;
 		try{
 		Statement stmt = SmartHealthCareSystem.con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT Name FROM Doctor");
-		while (rs.next()) {
-			  String DName = rs.getString("Name");
-			  System.out.println(i+". "+DName + "\n");
-			  i++;
+		boolean status = stmt.execute("SELECT Name, Did FROM Doctor");
+		if(status){
+			ResultSet rs = stmt.getResultSet();
+			while (rs.next()) {
+				String DName = rs.getString("Name");
+				int id=rs.getInt("Did");
+				System.out.println(id+". "+DName + "\n");
 			}
-		System.out.println("Enter Doctor ID for more details: ");
-		int did=SmartHealthCareSystem.nextint();
-		ResultSet detail = stmt.executeQuery("SELECT * FROM Doctor where Did="+did);
-        while (detail.next()) {
-           int id = detail.getInt("Did");
-           String name = detail.getString("Name");
-           dob = detail.getDate("DOB");
-           String gen = detail.getString("Gender");
-           String add = detail.getString("Address");
-           String cno = detail.getString("ContactNo");
-           String pass = detail.getString("Password");
-           int dpid = detail.getInt("DeptId");
-           String rank = detail.getString("Rank");
-           String sur = detail.getString("Surgeon");
-           int fee = detail.getInt("OpdFees");
-           System.out.println("ID: "+id);
-           System.out.println("Name: "+name);
-           System.out.println("DOB: "+dob.toString());
-           System.out.println("Gender: "+gen);
-           System.out.println("Address: "+add);
-           System.out.println("Contact No: "+cno);
-           System.out.println("Password: "+pass);
-           System.out.println("Department ID: "+dpid);
-           System.out.println("Rank: "+rank);
-           System.out.println("Surgeon: "+sur);
-           System.out.println("OPD Fees: "+fee+" Rs");
+			System.out.println("Enter Doctor ID for more details or press 0 to exit: ");
+			int did=SmartHealthCareSystem.nextint();
+			ResultSet detail = stmt.executeQuery("SELECT * FROM Doctor where Did="+did);
+			while (detail.next()) {
+				int id = detail.getInt("Did");
+				String name = detail.getString("Name");
+				dob = detail.getDate("DOB");
+				String gen = detail.getString("Gender");
+				String add = detail.getString("Address");
+				String cno = detail.getString("ContactNo");
+				String pass = detail.getString("Password");
+				int dpid = detail.getInt("DeptId");
+				String rank = detail.getString("Rank");
+				String sur = detail.getString("Surgeon");
+				int fee = detail.getInt("OpdFees");
+				System.out.println("ID: "+id);
+				System.out.println("Name: "+name);
+				System.out.println("DOB: "+dob.toString());
+				System.out.println("Gender: "+gen);
+				System.out.println("Address: "+add);
+				System.out.println("Contact No: "+cno);
+				System.out.println("Password: "+pass);
+				System.out.println("Department ID: "+dpid);
+				System.out.println("Rank: "+rank);
+				System.out.println("Surgeon: "+sur);
+				System.out.println("OPD Fees: "+fee+" Rs");
+			}
         }
+		else{
+			System.out.println("Error in getting Data");
+		}
 		}
 		catch(Exception e){
-			System.out.println(e);
+			System.out.println("Exception " + e.getMessage().toString());
 		}
 		
 	}
@@ -66,24 +83,34 @@ public class Admin extends Person
 		int i=1;
 		try{
 			Statement stmt = SmartHealthCareSystem.con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT Name FROM patient");
-			while (rs.next()) {
+			boolean status = stmt.execute("SELECT Name, Pid FROM patient");
+			if(status){
+				ResultSet rs = stmt.getResultSet();
+				while (rs.next()) {
 				  String pName = rs.getString("Name");
-				  System.out.println(i+". "+pName + "\n");
-				  i++;
+				  int pid=rs.getInt("Pid");
+				  System.out.println(pid+". "+pName + "\n");
 				}
-			System.out.println("Enter your choice: ");
-			System.out.println("1.View patient personal details\n2.View patient history\n");
-			int c=SmartHealthCareSystem.nextint();
-			if(c==1){
-				showPatientDetails();
+				int c=0;
+				while(c!=3){
+					System.out.println("Enter your choice: ");
+					System.out.println("1.View patient personal details\n2.View patient history\n3.Exit\n");
+					c=SmartHealthCareSystem.nextint();
+					if(c==1){
+						showPatientDetails();
+					}
+					else if(c==2){
+						showPatientHistory();
+					}
+				}
 			}
-			else if(c==2){
-				showPatientHistory();
+			else
+			{
+				System.out.println("Error in getting Data");
 			}
 		}
 		catch(Exception e){
-			System.out.println(e);
+			System.out.println("Exception " + e.getMessage().toString());
 		}
 	}
 	void registerDoctor()
@@ -102,7 +129,7 @@ public class Admin extends Person
         System.out.println("Contact No: ");
         String cno=SmartHealthCareSystem.nextintString();
         System.out.println("Password: ");
-        int pass=sc.nextInt();
+        int pass=sc.SmartHealthCareSystem.nextint();
         System.out.println("Department ID: ");
         String depid=sc.nextLine();
         System.out.println("Rank: ");
@@ -113,11 +140,18 @@ public class Admin extends Person
         int fee=SmartHealthCareSystem.nextint();
         try{
         	Statement stmt = SmartHealthCareSystem.con.createStatement();
-    		ResultSet rs = stmt.executeQuery("Insert into Doctor values("+id+", "+name+", "+dob+", "+gen+", "+add+", "+cno+", "+pass+", "+depid+", "+rank+", "+sur+", "+fee+")");
-    		System.out.println("Doctor has been successfully registered.");
+        	boolean status = statement.execute("Insert into Doctor values("+id+", "+name+", "+dob+", "+gen+", "+add+", "+cno+", "+pass+", "+depid+", "+rank+", "+sur+", "+fee+")");
+        	if(status)
+        	{
+        		System.out.println("Doctor has been successfully registered.");
+        	}
+        	else
+        	{
+        		System.out.println("Could not register doctor. Please try again");
+        	}
         }
         catch(Exception e){
-        	System.out.println(e);
+        	System.out.println("Exception " + e.getMessage().toString());
         }
 
 	}
@@ -129,8 +163,22 @@ public class Admin extends Person
 		int did=SmartHealthCareSystem.nextint();
 		try{
 			Statement stmt = SmartHealthCareSystem.con.createStatement();
-			ResultSet detail = stmt.executeQuery("UPDATE record SET Did="+did+" WHERE RecId="+rno);
-			System.out.println("You have successfully reassiged doctor to Record No: "+rno);
+			String query = "update table patient set"
+					+" name=" + getName()
+					+ " dob=" + getDob()
+					+ " gender=" +getGender()
+					+ " address=" + getAddress()
+					+ " contactno=" + getPhoneNumber()
+					+ " password=" + getPassword()
+					+" where pid="+ getPid();
+		            int count = statement.executeUpdate(query);
+					if(count == 1){
+		                System.out.println("Record has been updated successfully");
+		            }
+		            else
+		            {
+		            	System.out.println("Record not updated.Please try again");
+		            }
 		}
 		catch(Exception e){
 			System.out.println(e);
@@ -150,10 +198,10 @@ public class Admin extends Person
 			String p=sc.nextLine();
 			this.loginCredentials.setId(u);
 			this.loginCredentials.setPassword(p);
-			System.out.println("Credentials successfully updated!");
-			
+			System.out.println("Credentials successfully updated!");	
 		}
-		else{
+		else
+		{
 			System.out.println("Invalid username or password!");
 		}
 		
@@ -186,7 +234,9 @@ public class Admin extends Person
 		int i=SmartHealthCareSystem.nextint();
 		try{
 			Statement stmt = SmartHealthCareSystem.con.createStatement();
-			ResultSet detail = stmt.executeQuery("SELECT * FROM patient where Pid="+i);
+			boolean status = statement.execute("SELECT * FROM patient where Pid="+i);
+			if(status){
+                ResultSet detail= stmt.getResultSet();
 			while (detail.next()) {
 		           int id = detail.getInt("Pid");
 		           String name = detail.getString("Name");
@@ -203,9 +253,14 @@ public class Admin extends Person
 		           System.out.println("Contact No: "+cno);
 		           System.out.println("Password: "+pass);
 		        }
+			}
+			else
+			{
+				System.out.println("Error in getting Data");
+			}
 		}
 		catch(Exception e){
-			System.out.println(e);
+			System.out.println("Exception " + e.getMessage().toString());
 		}
 	}
 	void showPatientHistory(){
@@ -215,9 +270,11 @@ public class Admin extends Person
 		int i=SmartHealthCareSystem.nextint();
 		try{
 			Statement stmt = SmartHealthCareSystem.con.createStatement();
-			ResultSet detail = stmt.executeQuery("SELECT * FROM record where Pid="+i);
-			System.out.println("History for patient ID: "+i+"is shown below: ");
-			while (detail.next()) {
+			boolean status = statement.execute("SELECT * FROM record where Pid="+i);
+			if(status){
+                ResultSet detail= stmt.getResultSet();
+                System.out.println("History for patient ID: "+i+"is shown below: ");
+                while (detail.next()) {
 				   int recid=detail.getInt("RecId");
 				   int pid=detail.getInt("Pid");
 				   adate=detail.getDate("Admit_Date");
@@ -234,9 +291,13 @@ public class Admin extends Person
 		           System.out.print("Location: "+loc);
 		           System.out.print("Doctor ID: "+did);
 		        }
+			}
+			else{
+				System.out.println("Error in getting Data");
+			}
 		}
 		catch(Exception e){
-			System.out.println(e);
+			System.out.println("Exception " + e.getMessage().toString());
 		}
 	}
 }
